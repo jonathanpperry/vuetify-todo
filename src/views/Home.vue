db0287fe7f85fd0667954fb4c3fee7ca0b43b212
Jonathan Perry <jpper@umich.edu>
2019-10-11 23:39:45 +0900

    Did buttons with breakpoints and visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <v-btn class="pink white--text">Click Me</v-btn>
-    <v-btn flat color="pink">Click Me</v-btn>
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>home</v-icon>
-      <span>Email me</span>
-    </v-btn>
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>Email me</span>
-    </v-btn>
+    <v-btn class="hidden-sm-only">Click ME!</v-btn>
   </div>
 </template>
 
