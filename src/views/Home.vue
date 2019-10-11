616410efb1abd86c6ef245561dc2ca56d0fb0223
Jonathan Perry <jpper@umich.edu>
2019-10-11 23:31:22 +0900

    Did buttons & icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p
-      class="red white--text"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates impedit et accusamus alias exercitationem? Sed voluptate pariatur nisi eius possimus odit, qui voluptatibus quia rerum saepe cumque placeat a nihil.</p>
-    <p
-      class="pink lighten-4 red--text text--darken-4"
-    >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum ad culpa voluptates suscipit odit sapiente, aperiam ex nisi a et aliquid. Id nam rem labore! Aspernatur nesciunt eaque possimus accusamus?</p>
-    <h1 class="display-3">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    <v-btn class="pink white--text">Click Me</v-btn>
+    <v-btn flat color="pink">Click Me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>home</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>Email me</span>
+    </v-btn>
   </div>
 </template>
 
