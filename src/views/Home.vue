385eea1092aa3b7b940f801e403b297043c0dce6
Jonathan Perry <jpper@umich.edu>
2019-10-11 20:39:46 +0900

    Went through text and colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
-<div class="home">
-  <h1>Homepage</h1>
-</div>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates impedit et accusamus alias exercitationem? Sed voluptate pariatur nisi eius possimus odit, qui voluptatibus quia rerum saepe cumque placeat a nihil.</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum ad culpa voluptates suscipit odit sapiente, aperiam ex nisi a et aliquid. Id nam rem labore! Aspernatur nesciunt eaque possimus accusamus?</p>
+    <h1 class="display-3">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption">This is a caption</p>
+  </div>
 </template>
 
 <script>
-
-export default {
-};
+export default {};
 </script>
